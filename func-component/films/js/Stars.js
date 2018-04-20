42bb87f5b8b61baa52a8985c528e68182adfc1e9
Iluschenko Kirill <k.iluschenko@gmail.com>
2018-04-21 00:54:47 +0300

    Task 1. Complited

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,18 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function Stars({count}) {
+  return <ul className="card-body-stars u-clearfix">{ count >= 1 && count <=5 &&  typeof count === 'number' && CalcStars(count) }</ul>;
+}
+
+Stars.defaultProps = { stars: 0 }; //значение по умолчанию
+
+function CalcStars(count) {
+	let i = 0;
+	let listStars = [];
+	while (i != count) {
+		listStars.push(<li><Star /></li>)
+		i++
+	}
+	
+	return listStars;
 }
