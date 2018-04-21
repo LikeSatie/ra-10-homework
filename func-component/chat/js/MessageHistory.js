9027f747a2cdc47181d4fa9e0e0d8f01235a2e57
Kirill Iluschenko <k.iluschenko@mail.ru>
2018-04-21 18:34:53 +0300

    Task 1.2.2 Completed

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function MessageHistory({list}) {
+
+	console.log(list);
+	const messageList = list.map(message => {
+		return (
+ 			message.type === 'message' &&  <Message from={message.from} message={message} /> || 
+ 			message.type === 'response' && <Response from={message.from} message={message} /> || 
+ 			message.type === 'typing' && <Typing from={message.from} message={message} />) ;
+	})
+
+		return (<ul> {messageList} </ul>)
+}
