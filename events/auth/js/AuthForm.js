fbd96bd8ededd4ff5b761b5dc41c1a632a3b8a3c
Iluschenko Kirill <k.iluschenko@gmail.com>
2018-04-26 22:50:58 +0300

    Task 1.3.3 Completed

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const AuthForm = onAuth => {
+  let nameField, emailField, passwordField;
+  
+  const authUser = event => {
+    event.preventDefault();
+    if ((!onAuth) || (typeof onAuth !== 'function')) return null;
+        const userAuth = {  
+            name: nameField.value,
+            email: emailField.value,
+            password: passwordField.value
+        };
+        onAuth(userAuth);
+    }
+  
+  function checkEmail(event) {
+        event.currentTarget.value = event.currentTarget.value.replace(/[^\w@\.-]+/gi, "");
+    }
+    
+    function checkPassword(event) {
+        event.currentTarget.value = event.currentTarget.value.replace(/[^\w]+/gi, "");
+    }
+  
+  
+    return (
+        <form className="ModalForm" onSubmit={authUser} action="/404/auth/" method="POST">
+            <div className="Input">
+                <input ref={elem => nameField = elem} required type="text" placeholder="Имя" />
+                <label></label>
+            </div>
+            <div className="Input">
+                <input ref={elem => emailField = elem} onChange={checkEmail} type="email" placeholder="Электронная почта" />
+                <label></label>
+            </div>
+            <div className="Input">
+                <input ref={elem => passwordField = elem} onChange={checkPassword} required type="password" placeholder="Пароль" />
+                <label></label>
+            </div>
+            <button type="submit">
+                <span>Войти</span>
+                <i className="fa fa-fw fa-chevron-right"></i>
+            </button>
+        </form>
+    );
+}
