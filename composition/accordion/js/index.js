c31325b2fd316e775af5bedeeecc8dfe79be1814
Kirill Iluschenko <k.iluschenko@gmail.com>
2018-05-04 21:57:56 +0300

    В разработке. Компонент Section

--- composition/accordion/js/index.js
+++ composition/accordion/js/index.js
@@ -21,51 +21,35 @@ class App extends React.Component {
 	render (){
 		return (
 		<main className="main">
-      <h2 className="title">React</h2>
-      <Section />
-      <section className="section">
-        <button>toggle</button>
-        <h3 className="sectionhead">Выучил раз, используй везде!</h3>
-        <div className="articlewrap">
-          <div className="article">
-            После изучения React вы сможете использовать его концепции не только в браузере, но также и при разработке мобильных приложений с использованием React Native.
-          </div>
-        </div>
-      </section>
-      <section className="section">
-        <button>toggle</button>
-        <h3 className="sectionhead">Использование JSX</h3>
-        <div className="articlewrap">
-          <div className="article">
-            JSX является языком, расширяющим синтаксис стандартного Javascript. По факту он позволяет писать HTML-код в JS-скриптах. Такой подход упрощает разработку компонентов и повышает читаемость кода.
-          </div>
-        </div>
-      </section>
-    </main>
-    )
+			<h2 className="title">React</h2>
+
+			{ content.map((item, itemIndex) => {
+        return (
+       		<Section title={item.title} article={item.article} /> 
+        )
+   		}) }
+		</main>
+		)
 
 	}
 }
 
 class Section extends React.Component {
-
-	render() {
-		
-		
-		return (
-			<section className="section open">
-        <button>toggle</button>
-        <h3 className="sectionhead">Компоненты</h3>
-        <div className="articlewrap">
-          <div className="article">
-            Каждый компонент являются законченной частью пользовательского интерфейса и сам управляет своим состоянием, а композиция компонентов (соединение) позволяет создавать более сложные компоненты. Таким образом, создается иерархия компонентов, причем каждый отдельно взятый компонент независим сам по себе. Такой подход позволяет строить сложные интерфейсы, где есть множество состояний, и взаимодействовать между собой.
-          </div>
-        </div>
-      </section>	
-			)
-		
+	constructor (props) {
+		super(props)
 	}
 
-
+	render() {
+		return (
+			<section className="section open">
+				<button>toggle</button>
+				<h3 className="sectionhead">{this.props.title}</h3>
+				<div className="articlewrap">
+					<div className="article">{this.props.article}
+					</div>
+				</div>
+			</section>	
+		)
+	}
 }
 
