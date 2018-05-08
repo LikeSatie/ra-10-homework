711f99c0e9dee3b856e138f5e76f509186a3efd0
Kirill Iluschenko <k.iluschenko@gmail.com>
2018-05-09 02:14:11 +0300

    2.3.3. Completed

--- ctrl-component/tshirt/js/FontSelector.js
+++ ctrl-component/tshirt/js/FontSelector.js
@@ -1,7 +1,41 @@
-const FontSelector = ({fonts, selectedFont, onSelect}) => {
-    return (
-        <div className="font-picker">
-            Выберите шрифт
-        </div>
-    )
-};
+class FontSelector extends React.Component {
+	constructor (props) {
+		 super(props);
+		 
+	}
+
+	checkedFont(item){
+		this.props.onSelect(item);
+	}
+	
+	render() {
+		return (
+			<div>
+				{ fonts.map((item, i) => {
+					return(
+						<div className="grid center font-item">     
+							<input type="radio" name="font" value={item.name} id={item.name} onChange={this.checkedFont.bind(this, item)} />
+							<label htmlFor={item.name} className="grid-1">
+								<PictureFont text={item.name.slice(0, -1)} path={item.path} key={`char-${i}`} />
+							</label>  			
+						</div>
+					)
+				})}
+			</div>
+		)
+	}
+}
+
+
+
+// Второй вариант реализации функции, если не передавать аргумент item
+//<input type="radio" name="font" value={item.name} id={item.name} onChange={this.checkedFont.bind(this)} />
+
+/*	checkedFont(event, r){
+		const chekFont = fonts.filter(item => {
+  		return (item.name === event.currentTarget.value);
+		})	
+		this.props.onSelect(chekFont[0]);
+
+		console.log(r)
+	}*/
--- ctrl-component/tshirt/js/TextRenderLine.js
+++ ctrl-component/tshirt/js/TextRenderLine.js
@@ -1,7 +1,35 @@
-const TextRenderLine = ({value, onChange}) => {
-	return (
+class TextRenderLine extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	text(event) {
+			this.props.onChange(event.currentTarget.value)
+	}
+
+
+	render() {
+		return (
 		<div className="type-text">
-			Наберите текст
+			<textarea 
+				name="text" 
+				id="font-text" 
+				cols="30" 
+				rows="2" 
+				placeholder="Введите текст для футболки"
+				onChange={this.text.bind(this)}
+
+			>
+			</textarea>
+			
 		</div>
 	);
+	}
+
+}
+
+
+/*const TextRenderLine = ({value, onChange}) => {
+	Наберите текст
 };
+*/
