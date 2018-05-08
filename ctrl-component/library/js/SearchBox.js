1d36c257af52873d94e165d497ceecee93f58b22
Kirill Iluschenko <k.iluschenko@gmail.com>
2018-05-08 23:54:06 +0300

    2.3.2. Completed

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,20 @@
-const SearchBox = () => {
-    return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
-    );
-};
+class SearchBox extends React.Component {
+	constructor (props) {
+		super(props);
+	}
+
+	filter(event) {
+		this.props.filterBooks(event.currentTarget.value);
+	}
+
+	render() {
+		 return (
+		<input 
+			type="text" 
+			placeholder="Поиск по названию или автору"
+			onChange={this.filter.bind(this)}
+			value={this.props.value}
+		/>
+	);
+	}
+}
