363d696966a37249541403683b81d4093875bfa7
Kirill Iluschenko <k.iluschenko@gmail.com>
2018-05-17 22:31:25 +0300

    Task 1.3.3 Исправления #3

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,46 +1,46 @@
 'use strict';
 
 const AuthForm = onAuth => {
-  let nameField, emailField, passwordField;
-  
+ 
   const authUser = event => {
-    event.preventDefault();
-    if ((!onAuth) || (typeof onAuth !== 'function')) return null;
-        const userAuth = {  
-            name: nameField.value,
-            email: emailField.value,
-            password: passwordField.value
-        };
-        onAuth(userAuth);
-    }
+	event.preventDefault();
+	if ((!onAuth) || (typeof onAuth !== 'function')) return null;
+	const formElements = event.currentTarget.elements;
+ 
+		const userAuth = {  
+			name: formElements.name.value,
+			email: formElements.email.value,
+			password: formElements.password.value
+		};
+		onAuth(userAuth);
+	}
   
   function checkEmail(event) {
-        event.currentTarget.value = event.currentTarget.value.replace(/[^\w@\.-]+/gi, "");
-    }
-    
-    function checkPassword(event) {
-        event.currentTarget.value = event.currentTarget.value.replace(/[^\w]+/gi, "");
-    }
+		event.currentTarget.value = event.currentTarget.value.replace(/[^\w@\.-]+/gi, "");
+	}
+	
+	function checkPassword(event) {
+		event.currentTarget.value = event.currentTarget.value.replace(/[^\w]+/gi, "");
+	}
   
-  
-    return (
-        <form className="ModalForm" onSubmit={authUser} action="/404/auth/" method="POST">
-            <div className="Input">
-                <input ref={elem => nameField = elem} required type="text" placeholder="Имя" />
-                <label></label>
-            </div>
-            <div className="Input">
-                <input ref={elem => emailField = elem} onChange={checkEmail} type="email" placeholder="Электронная почта" />
-                <label></label>
-            </div>
-            <div className="Input">
-                <input ref={elem => passwordField = elem} onChange={checkPassword} required type="password" placeholder="Пароль" />
-                <label></label>
-            </div>
-            <button type="submit">
-                <span>Войти</span>
-                <i className="fa fa-fw fa-chevron-right"></i>
-            </button>
-        </form>
-    );
+	return (
+		<form className="ModalForm" onSubmit={authUser} action="/404/auth/" method="POST">
+			<div className="Input">
+				<input required type="text" placeholder="Имя" name="name" />
+				<label></label>
+			</div>
+			<div className="Input">
+				<input onChange={checkEmail} type="email" placeholder="Электронная почта" name="email" />
+				<label></label>
+			</div>
+			<div className="Input">
+				<input onChange={checkPassword} required type="password" placeholder="Пароль" name="password" />
+				<label></label>
+			</div>
+			<button type="submit">
+				<span>Войти</span>
+				<i className="fa fa-fw fa-chevron-right"></i>
+			</button>
+		</form>
+	);
 }
